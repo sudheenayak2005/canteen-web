b25062033363f2dff03c01eccea4bc40ba0ac4f9
sudheer <sudheernayak72@gmail.com>
2025-12-06 08:23:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import calendar
 import csv
 import pytz
 from flask_cors import CORS
+ IST=pytz.timezone("Asia/Kolkata")
 app = Flask(__name__)
 CORS(app)
 app.secret_key = "c0e4ce9f2d5b4475dfc9b49034f3e61a87b1c96c6b8a405ebcfa7c4df388e73a"
@@ -92,7 +93,7 @@ def admin_qr_page():
 # ============================================================
 #  HELPER: CURRENT SLOT (AUTO)
 # ============================================================
- IST=pytz.timezone("Asia/Kolkata")
+
 def get_current_slot():
    now=datetime.now(IST)
     h = now.hour
@@ -1006,6 +1007,7 @@ def export_logs():
 if __name__ == "__main__":
     # debug=True for local testing
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
